838a185c5e3f9778b748f1e69a6e3126f5b93da3
shloop <spindlerjonas@gmail.com>
2024-08-19 21:12:13 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# mobi2cbz
-mobi2cbz
+# kindle2cbz
+
+### Purpose
+
+This is a very simple tool which extracts all images from an **unencrypted** eBook in MOBI format (.mobi or .azw3) and 
+places them in a CBZ archive. Images in CBZ file will appear in the order they exist as PDB records in the MOBI file, 
+except that the cover will be moved to the start and the thumbnail will be omitted.
+
+I created this so that I would have a simpler alternative to using
+[KindleUnpack](https://github.com/kevinhendricks/KindleUnpack) to convert my comic books in Kindle format into CBZ so 
+that I can read them in my preferred software. 
+
+### Basic Setup and Usage
+
+An x64 Windows binary is provided in the [releases](https://github.com/shloop/kindle2cbz/releases) section. 
+Alternatively, if you have a [Rust development environment](https://www.rust-lang.org/tools/install) set up, you can 
+install with Cargo by simply running:
+
+```
+cargo install kindle2cbz
+``` 
+
+To convert a Kindle eBook to CBZ with no options, run:
+
+```
+kindle2cbz <PATH_TO_EBOOK>
+```
+
+where <PATH_TO_EBOOK> is the path to a valid unencrypted eBook in MOBI format. If successful, this will output a CBZ 
+file with the same base name in the same directory as the input file. For instance, if the input file was `comic.azw3`, 
+there would be a new file named `comic.cbz` in the same directory containing the extracted images.
+
+### Note on CBZ Format
+
+A CBZ file is actually just a standard .zip file containing images with a different extension. If you wish, you can 
+extract the images from a generated CBZ file by renaming it to .zip and opening it in a file explorer. When using this
+program you can also specify an output filename with .zip as the extension if you ust want the images.
+
+### All Options
+
+```
+Usage: kindle2cbz [OPTIONS] <IN_FILE> [OUT_FILE]
+
+Arguments:
+  <IN_FILE>   Path to mobi file to convert
+  [OUT_FILE]  Path to save CBZ to. If omitted, original path and base filename will be used
+
+Options:
+  -f, --force    If set, will overwrite any existing file with same path as output
+  -h, --help     Print help
+  -V, --version  Print version
+```
---
+++ src/main.rs
@@ -0,0 +1,118 @@
+use clap::Parser;
+use mobi::Mobi;
+use mobi::headers::ExthRecord;
+use std::io::{BufRead, Write};
+use std::fs;
+use std::process::ExitCode;
+use zip::write::SimpleFileOptions;
+use zip::ZipWriter;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to mobi file to convert
+    #[arg()]
+    in_file: String,
+
+    /// Path to save CBZ to. If omitted, original path and base filename will be used.
+    #[arg()]
+    out_file: Option<String>,
+
+    /// If set, will overwrite any existing file with same path as output.
+    #[arg(short, long, default_value_t = false)]
+    force: bool,
+}
+
+fn main() -> ExitCode {
+
+    // Parse command line args
+    let args = Args::parse();
+
+    // Try to parse input book
+    let m = Mobi::from_path(&args.in_file).expect("Failed to parse input file.");
+
+    // Infer output path if one isn't provided
+    let out_file = match args.out_file {
+        Some(x) => x,
+        None => {
+            let p = std::path::Path::new(&args.in_file);
+            if let Some(ext) = p.extension() {
+                // Replace extension with ".cbz"
+                p.to_str().unwrap().to_string().replace(
+                    ext.to_str().unwrap(), "cbz")
+            } else {
+                // No extension => append ".cbz" to original filename
+                args.in_file.to_string() + ".cbz"
+            }
+        }
+    };
+
+    // If output file already exists, prompt user to overwrite unless force option is used
+    if std::path::Path::new(&out_file).exists() && !args.force {
+        loop {
+            print!("{} already exists. Do you wish to overwrite? y/N:", &out_file);
+            std::io::stdout().flush().unwrap();
+            let mut line = String::new();
+            std::io::stdin().lock().read_line(&mut line).unwrap();
+            match line.trim().to_lowercase().as_str() {
+                "y" => { break; }
+                "n" => { return ExitCode::FAILURE; }
+                _ => { continue; }
+            }
+        }
+    }
+
+
+    // Check if there are images marked as cover or thumbnail
+    let mut cover_offset = -1;
+    let mut thumb_offset = -1;
+    for x in &m.metadata.exth.records {
+        match x.0 {
+            ExthRecord::CoverOffset => {
+                cover_offset = i32::from_be_bytes(<[u8; 4]>::try_from(&x.1[0][0..4]).unwrap());
+            }
+            ExthRecord::ThumbOffset => {
+                thumb_offset =
+                    i32::from_be_bytes(<[u8; 4]>::try_from(&x.1[0][0..4]).unwrap()) - 1;
+            }
+            _ => {}
+        }
+    }
+
+    // Begin image extraction
+
+    println!("Extracting images from {}...", m.title());
+
+    // Open file for writing
+    let file = fs::File::create(out_file).expect("Failed to create output file.");
+    let mut zip = ZipWriter::new(file);
+    let options =
+        SimpleFileOptions::default().compression_method(zip::CompressionMethod::Deflated);
+
+    // Page number, incremented for each image that is not the cover or thumbnail
+    let mut i_page = 0;
+    // Total offset, incremented with each image
+    let mut i_offset = -1;
+    for x in m.image_records() {
+        i_offset += 1;
+
+        let filename = if i_offset == cover_offset {
+            // Cover => cover.ext
+            "out/cover.jpg".to_string()
+        } else if i_offset == thumb_offset {
+            // Skip thumbnail
+            continue
+        } else {
+            // Page => pXXXX.ext
+            i_page += 1;
+            std::format!("out/p{:0>4}.jpg", i_page)
+        };
+
+        zip.start_file(filename, options).expect("Failed to add file to CBZ.");
+        zip.write_all(&x.content).expect("Failed to add file to CBZ.");
+    }
+
+    zip.finish().expect("Failed to finalize CBZ file.");
+    println!("Done");
+    ExitCode::SUCCESS
+}
